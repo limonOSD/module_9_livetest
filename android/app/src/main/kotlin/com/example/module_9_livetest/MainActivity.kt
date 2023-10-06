c442877d5f80bb29dfd67887a1f1ee49f02053c1
limon <limonpaulargo47@gmail.com>
2023-10-06 23:32:09 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/module_9_livetest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.module_9_livetest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Live test',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> buttonList = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+  String selectedButton = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Size Selector'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0), // Add padding around the GridView
+          child: Wrap(
+            spacing: 8.0, // Add space between buttons
+            runSpacing: 8.0,
+            children: buttonList.map((buttonName) {
+              bool isSelected = buttonName == selectedButton;
+
+              return ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  primary: isSelected ? Colors.orange : Colors.grey,
+                ),
+                onPressed: () {
+                  setState(() {
+                    selectedButton = buttonName;
+                  });
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Selected text size: $buttonName'),
+                    ),
+                  );
+                },
+                child: Text(buttonName),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
